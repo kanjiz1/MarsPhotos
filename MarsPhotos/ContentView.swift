e0709b1c2d172f713d6010b29c2b6b7a106453c0
Kanjiz1 <kanjis007@gmail.com>
2020-06-25 22:55:00 -0500

    Initial Commit

---
+++ MarsPhotos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MarsPhotos
+//
+//  Created by Oforkanji Odekpe on 6/25/20.
+//  Copyright © 2020 Oforkanji Odekpe. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
